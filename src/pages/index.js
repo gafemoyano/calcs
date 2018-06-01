cce57716061741ad218399eebbce2c570ee0b51d
Felipe Moyano <gafemoyano@gmail.com>
2018-06-01 15:50:25 -0500

    calc stuff

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,92 +1,115 @@
-import React from "react";
-import Link from "gatsby-link";
+import React, { Component } from 'react'
+import Link from 'gatsby-link'
 
-const IndexPage = () => (
-  <form className="mx-auto md:w-1/2">
-    <label
-      className="block font-bold mb-2 text-xs uppercase"
-      htmlFor="first-name"
-    >
-      What's the purchase price of the home?
-    </label>
-    <input
-      className="appearance-none block bg-grey-lighter mb-6 p-3 rounded-md text-grey-darker w-full"
-      id="first-name"
-      type="text"
-      placeholder="$3.000.000"
-      type="text"
-    />
+class IndexPage extends Component {
+  state = {
+    homePrice: '300000',
+    downPayment: '0',
+    downPaymentPercent: '',
+    loanProgram: '30',
+    interestRate: '4.368'
+  }
 
-    <label
-      className="block font-bold mb-2 text-xs uppercase"
-      htmlFor="last-name"
-    >
-      How much do you want to put down?
-    </label>
-    <input
-      className="appearance-none block bg-grey-lighter mb-6 p-3 rounded-md text-grey-darker w-full"
-      id="last-name"
-      type="text"
-      placeholder="$30.000"
-    />
-    <p
-      className="block font-bold mb-2 text-xs uppercase"
-    >
-      Are you a veteran or currently serving in the military?
-    </p>
-    <div className="block mb-6">
-      <div className="inline mr-2">
+  handleChange = event => {
+    this.setState({
+      [event.target.name]: event.target.value
+    })
+  }
+
+  calculateMortage = () => {
+    let homePrice = parseInt(this.state.homePrice, 10)
+    let downPayment = parseInt(this.state.downPayment, 10)
+    let loanProgram = parseInt(this.state.loanProgram, 10)
+
+    console.log(homePrice)
+    console.log(downPayment)
+    console.log(loanProgram)
+
+    let monthlyRate = this.monthlyRate()
+    console.log(monthlyRate)
+
+    let loanValue = homePrice - downPayment
+    let pow = Math.pow(1 + monthlyRate, loanProgram * 12)
+    let mortage = loanValue * ((monthlyRate * pow) / pow - 1)
+    if (mortage !== null && mortage !== undefined && mortage !== NaN) {
+      return mortage
+    } else {
+      return 0
+    }
+  }
+
+  monthlyRate() {
+    let interestRate = parseFloat(this.state.interestRate)
+    return interestRate / parseFloat(12)
+  }
+
+  render() {
+    return (
+      <form className="mx-auto md:w-1/2">
+        <label
+          className="block font-bold mb-2 text-xs uppercase"
+          htmlFor="first-name">
+          What's the purchase price of the home?
+        </label>
         <input
-          id="yes"
-          type="radio"
-          value={true}
-          className="mr-1"
+          className="appearance-none block bg-grey-lighter mb-6 p-3 rounded-md text-grey-darker w-full"
+          id="first-name"
+          type="text"
+          placeholder="$3.000.000"
+          type="text"
+          name="homePrice"
+          value={this.state.homePrice}
+          onChange={this.handleChange}
         />
         <label
-          className="font-bold mb-2 text-xs uppercase"
-          htmlFor="yes">Yes</label>
-      </div>
-      <div className="inline ml-2">
+          className="block font-bold mb-2 text-xs uppercase"
+          htmlFor="last-name">
+          How much do you want to put down?
+        </label>
         <input
-          id="yes"
-          type="radio"
-          value={true}
-          className="mr-1"
+          className="appearance-none block bg-grey-lighter mb-6 p-3 rounded-md text-grey-darker w-full"
+          id="last-name"
+          type="text"
+          placeholder="$30.000"
+          name="downPayment"
+          value={this.state.downPayment}
+          onChange={this.handleChange}
         />
         <label
-          className="font-bold mb-2 text-xs uppercase"
-          htmlFor="no">No</label>
-      </div>
-    </div>
+          className="block font-bold mb-2 text-xs uppercase"
+          htmlFor="zip-code">
+          What's your loan program in years?
+        </label>
+        <input
+          className="appearance-none block bg-grey-lighter mb-6 p-3 rounded-md text-grey-darker w-full"
+          id="loanProgram"
+          type="text"
+          placeholder="1"
+          name="loanProgram"
+          value={this.state.loanProgram}
+          onChange={this.handleChange}
+        />
+        <label
+          className="block font-bold mb-2 text-xs uppercase"
+          htmlFor="credit">
+          What's your interest rate?
+        </label>
+        <input
+          className="appearance-none block bg-grey-lighter mb-6 p-3 rounded-md text-grey-darker w-full"
+          id="loanProgram"
+          type="text"
+          placeholder="1"
+          name="interestRate"
+          value={this.state.interestRate}
+          onChange={this.handleChange}
+        />
+        <div>{this.calculateMortage()}</div>
+        <button className="border-b-4 border-grey-darker hover:border-grey-dark bg-grey-dark hover:bg-grey font-bold px-6 py-3 rounded text-sm text-white">
+          Submit
+        </button>
+      </form>
+    )
+  }
+}
 
-    <label
-      className="block font-bold mb-2 text-xs uppercase"
-      htmlFor="zip-code"
-    >
-      What is your hom's ZIP code?
-    </label>
-    <input
-      className="appearance-none block bg-grey-lighter mb-6 p-3 rounded-md text-grey-darker w-full"
-      id="zip-code"
-      type="text"
-      placeholder="10001"
-    />
-    <label className="block font-bold mb-2 text-xs uppercase" htmlFor="credit">
-      How would you rate your credit?
-    </label>
-    <div className="block relative w-full">
-      <select className="mb-6 block appearance-none w-full bg-grey-lighter border border-grey-lighter text-grey-darker py-3 px-4 pr-8 rounded" id="grid-state">
-        <option>Excellent</option>
-        <option>Average</option>
-        <option>Poor</option>
-      </select>
-      <div className="pointer-events-none absolute pin-y pin-r flex items-center px-2 text-grey-darker">
-        <svg className="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z" /></svg>
-      </div></div>
-    <button className="border-b-4 border-grey-darker hover:border-grey-dark bg-grey-dark hover:bg-grey font-bold px-6 py-3 rounded text-sm text-white">
-      Submit
-    </button>
-  </form>
-);
-
-export default IndexPage;
+export default IndexPage

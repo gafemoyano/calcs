e9543626b05fe65ea6efda2f51d35a814ea33c92
Felipe Moyano <gafemoyano@gmail.com>
2018-06-04 17:10:59 -0500

    whatever

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react'
 import Link from 'gatsby-link'
+import CurrencyInput from 'react-currency-input'
+import { SSL_OP_PKCS1_CHECK_1 } from 'constants'
 
 class IndexPage extends Component {
   state = {
-    homePrice: '300000',
-    downPayment: '0',
+    homePrice: '315000',
+    downPayment: '50000',
     downPaymentPercent: '',
     loanProgram: '30',
     interestRate: '4.368'
@@ -30,7 +32,7 @@ class IndexPage extends Component {
 
     let loanValue = homePrice - downPayment
     let pow = Math.pow(1 + monthlyRate, loanProgram * 12)
-    let mortage = loanValue * ((monthlyRate * pow) / pow - 1)
+    let mortage = loanValue * ((monthlyRate * pow) / (pow - 1))
     if (mortage !== null && mortage !== undefined && mortage !== NaN) {
       return mortage
     } else {
@@ -40,74 +42,88 @@ class IndexPage extends Component {
 
   monthlyRate() {
     let interestRate = parseFloat(this.state.interestRate)
-    return interestRate / parseFloat(12)
+    return interestRate / 100 / parseFloat(12)
   }
 
   render() {
+    let mortage = this.calculateMortage()
     return (
-      <form className="mx-auto md:w-1/2">
-        <label
-          className="block font-bold mb-2 text-xs uppercase"
-          htmlFor="first-name">
-          What's the purchase price of the home?
-        </label>
-        <input
-          className="appearance-none block bg-grey-lighter mb-6 p-3 rounded-md text-grey-darker w-full"
-          id="first-name"
-          type="text"
-          placeholder="$3.000.000"
-          type="text"
-          name="homePrice"
-          value={this.state.homePrice}
-          onChange={this.handleChange}
-        />
-        <label
-          className="block font-bold mb-2 text-xs uppercase"
-          htmlFor="last-name">
-          How much do you want to put down?
-        </label>
-        <input
-          className="appearance-none block bg-grey-lighter mb-6 p-3 rounded-md text-grey-darker w-full"
-          id="last-name"
-          type="text"
-          placeholder="$30.000"
-          name="downPayment"
-          value={this.state.downPayment}
-          onChange={this.handleChange}
-        />
-        <label
-          className="block font-bold mb-2 text-xs uppercase"
-          htmlFor="zip-code">
-          What's your loan program in years?
-        </label>
-        <input
-          className="appearance-none block bg-grey-lighter mb-6 p-3 rounded-md text-grey-darker w-full"
-          id="loanProgram"
-          type="text"
-          placeholder="1"
-          name="loanProgram"
-          value={this.state.loanProgram}
-          onChange={this.handleChange}
-        />
-        <label
-          className="block font-bold mb-2 text-xs uppercase"
-          htmlFor="credit">
-          What's your interest rate?
-        </label>
-        <input
-          className="appearance-none block bg-grey-lighter mb-6 p-3 rounded-md text-grey-darker w-full"
-          id="loanProgram"
-          type="text"
-          placeholder="1"
-          name="interestRate"
-          value={this.state.interestRate}
-          onChange={this.handleChange}
-        />
-        <div>{this.calculateMortage()}</div>
-        <button className="border-b-4 border-grey-darker hover:border-grey-dark bg-grey-dark hover:bg-grey font-bold px-6 py-3 rounded text-sm text-white">
-          Submit
-        </button>
-      </form>
+      <div className="mx-auto md:w-1/2">
+        <h1 className="text-2xl mb-6">Mortgage Calculator </h1>
+        <form>
+          <label
+            className="block font-bold text-grey-darker mb-2 text-xs uppercase"
+            htmlFor="first-name"
+          >
+            What's the purchase price of the home?
+          </label>
+          <input
+            className="appearance-none block bg-yellow-lighter mb-6 p-3 rounded-md text-grey-darker w-full"
+            precision="0"
+            name="homePrice"
+            value={this.state.homePrice}
+            onChange={this.handleChange}
+          />
+          <label
+            className="block font-bold text-grey-darker mb-2 text-xs uppercase"
+            htmlFor="last-name"
+          >
+            How much do you want to put down?
+          </label>
+          <input
+            className="appearance-none block bg-yellow-lighter mb-6 p-3 rounded-md text-grey-darker w-full"
+            id="last-name"
+            type="number"
+            min="0"
+            placeholder="$30.000"
+            name="downPayment"
+            value={this.state.downPayment}
+            onChange={this.handleChange}
+          />
+          <label
+            className="block font-bold text-grey-darker mb-2 text-xs uppercase"
+            htmlFor="zip-code"
+          >
+            What's your loan program in years?
+          </label>
+          <input
+            className="appearance-none block bg-yellow-lighter mb-6 p-3 rounded-md text-grey-darker w-full"
+            id="loanProgram"
+            type="number"
+            min="0"
+            placeholder="1"
+            name="loanProgram"
+            value={this.state.loanProgram}
+            onChange={this.handleChange}
+          />
+          <label
+            className="block font-bold text-grey-darker mb-2 text-xs uppercase"
+            htmlFor="credit"
+          >
+            What's your interest rate?
+          </label>
+          <input
+            className="appearance-none block bg-yellow-lighter mb-6 p-3 rounded-md text-grey-darker w-full"
+            id="loanProgram"
+            type="text"
+            min="0"
+            name="interestRate"
+            value={this.state.interestRate}
+            onChange={this.handleChange}
+          />
+          <div className="my-16 border-yellow border-style-solid border-4 p-2">
+            <label
+              className="block font-bold text-grey-darkest mb-2 text-xl uppercase"
+              htmlFor="credit"
+            >
+              Your monthly rate
+            </label>
+            <div className="font-bold text-grey-darkest text-3xl">
+              ${mortage}
+            </div>
+          </div>
+        </form>
+      </div>
     )
   }
 }
